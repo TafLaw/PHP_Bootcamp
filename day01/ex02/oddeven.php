fedf7f0f4be7f7272bdc40bfe7e622b757419485
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-10-11 10:51:02 +0200

    oddeven

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -22,6 +22,8 @@
                 oddeven($x);
             else if (ctype_alpha($x))
                 echo "$x is not a number\n";
+            else
+                echo "$x is not a number\n";
         }
     }
     operate();
