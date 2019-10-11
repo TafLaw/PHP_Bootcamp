b075be9826bda5f53fdd3495225ba34cae286402
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-10-11 14:31:44 +0200

    rostring.php

---
+++ day01/ex07/rostring.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+$i = 1;
+$string = trim($argv[1]);
+$string = preg_replace('/\s+/', ' ', $string);
+$string = explode(" ", $string);
+while($string[$i] )
+{
+    echo $string[$i];
+    if ($i < count($string))
+    echo " ";
+    $i++;
+}
+if($argc != 1)
+echo $string[0]."\n";
+?>
