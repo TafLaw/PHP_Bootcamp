b7cee33266b97dcd2f84cc04beda435699f74be1
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-10-15 10:47:39 +0200

    create.php

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -13,11 +13,24 @@
         {
             if (!file_exists("./private") || !file_exists("./private/passwd"))
                 mkdir("private");
-            /* $file = fopen("account", "w");
-            fwrite($file, $user);
-            fwrite($file, $pass); */
+            if (file_exists("./private/passwd"))
+            {
+                $arr = unserialize(file_get_contents("./private/passwd"));
+                foreach ($arr as $value)
+                {
+                    if ($value["login"] === $user)
+                    {
+                        echo "ERROR\n";
+                        return;
+                    }
+                }
+            }
+            $new["login"] = $user;
+            $new["passwd"] = hash('md5', $pass);
+            $arr = array($new);
+            file_put_contents("./private/passwd", serialize($arr));
         }
-        echo "we're in\n";
+        echo "OK\n";
     }
     else
         echo "ERROR\n";
